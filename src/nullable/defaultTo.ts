a50009777b4f3467323b0fe2aa50b340183176ff
hiroqn <hiroqn1008@gmail.com>
2017-07-06 19:37:49 +0900

    :sparkles: add Nullable.defaultTo

---
+++ src/nullable/defaultTo.ts
@@ -0,0 +1,11 @@
+import { Nullable } from './Nullable';
+
+export function defaultTo<T>(x: T, n: Nullable<T>): T {
+    return n === null ? x : n
+}
+
+export function defaultToC<T>(x: T): (n: Nullable<T>) => T {
+    return function (n) {
+        return n === null ? x : n
+    }
+}
