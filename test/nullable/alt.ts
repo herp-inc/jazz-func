292e71e69742faf7823b726992b9dbb620dea6a4
Ryota Kameoka <kameoka.ryota@gmail.com>
2017-08-09 23:03:12 +0900

    Update test for Nullable.alt()

--- test/nullable/alt.ts
+++ test/nullable/alt.ts
@@ -2,20 +2,20 @@ import 'mocha';
 import * as assert from 'power-assert';
 import { alt } from '../../src/nullable/alt';
 
-describe('Nullable.zip()', () => {
-    it('should alt(null, null) to null', () => {
+describe('Nullable.alt()', () => {
+    it('should return null when the both arguments are null', () => {
         assert.equal(alt(null, null), null);
     });
 
-    it('should alt(non-null, null) to 42', () => {
+    it('should return the first argument when it is a non-null value and the second one is null', () => {
         assert.equal(alt(42, null), 42);
     });
 
-    it('should alt(null, non-null) to 42', () => {
+    it('should return the second argument when it is a non-null value and the first one is null', () => {
         assert.equal(alt(null, 42), 42);
     });
 
-    it('should alt(non-null-a, non-null-b) to non-null-a', () => {
+    it('should return the first argument when it is a non-null value and the second one is also non-null', () => {
         const value: number | string | null = alt(42, 'hello, world');
         assert.deepEqual(value, 42);
     });
